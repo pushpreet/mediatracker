301cb95aa8fd667ca8c7f446734ac330b733cc79
pushpreet <hanspal.pushpreet@gmail.com>
2018-04-02 01:33:01 +0530

    add base.html and got css working

--- mediatracker/settings.py
+++ mediatracker/settings.py
@@ -125,4 +125,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = ''
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
